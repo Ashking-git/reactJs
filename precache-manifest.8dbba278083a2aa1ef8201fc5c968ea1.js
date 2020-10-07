166989935e25f46fce66892420137d629dc81267
Ashking Flawless <ashkingflawless@Ashkings-MacBook-Pro.local>
2020-10-07 20:33:05 +0100

    Updates

---
+++ precache-manifest.8dbba278083a2aa1ef8201fc5c968ea1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6da67921a3a7b6e1eab02816ea2a8b7",
+    "url": "/reactJs/index.html"
+  },
+  {
+    "revision": "79cb1eae47b6bdf80425",
+    "url": "/reactJs/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "9c823e295725231652b4",
+    "url": "/reactJs/static/css/main.0ff7019f.chunk.css"
+  },
+  {
+    "revision": "79cb1eae47b6bdf80425",
+    "url": "/reactJs/static/js/2.4e68105f.chunk.js"
+  },
+  {
+    "revision": "706ae2ba09b6b4555454ecd6058d6910",
+    "url": "/reactJs/static/js/2.4e68105f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c823e295725231652b4",
+    "url": "/reactJs/static/js/main.41a9ca44.chunk.js"
+  },
+  {
+    "revision": "ce84dfbf37b8ac0c4768",
+    "url": "/reactJs/static/js/runtime-main.b379d829.js"
+  },
+  {
+    "revision": "bbd0b285ed23feffcb697b02bd5c8d7e",
+    "url": "/reactJs/static/media/about.bbd0b285.png"
+  },
+  {
+    "revision": "28f7328498032ffd75bb3597d5f1899d",
+    "url": "/reactJs/static/media/img1.28f73284.PNG"
+  },
+  {
+    "revision": "b70d841e8402bd4320231bf44e5097e1",
+    "url": "/reactJs/static/media/mark2.b70d841e.png"
+  },
+  {
+    "revision": "83399afee6c8af6b96b34496bbde3174",
+    "url": "/reactJs/static/media/seo.83399afe.png"
+  },
+  {
+    "revision": "0ebb1eeda9b42e65ee44c56eec488e6a",
+    "url": "/reactJs/static/media/web.0ebb1eed.png"
+  }
+]);
